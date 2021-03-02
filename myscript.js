3702727aa0bd103442275a934a1790160d9d27d9
EJM <erin.jay.moyer@gmail.com>
2021-03-01 20:56:57 -0700

    Cleaned up code. Added reset function. Started resize function.

--- myscript.js
+++ myscript.js
@@ -3,49 +3,60 @@ const container = document.getElementById("container");
 createGrid (); // for start button later
 
 //create 16 x 16 grid
-
 function createGrid () {
-var tileCount = 0; // keep track of tile to change? / give specific ID to each div
-for (k=0; k<16; k++){
-    // create ID count 
-    for (x =0; x<16; x++){
-        tileCount ++; 
-        const container = document.querySelector('#container');
-        const tile = document.createElement('div');
-       // content.classList.add('content');
-        tile.className = "etch";
-        tile.id = tileCount;
-        container.appendChild(tile);
-                        }
-                    } 
+    for (columns=0; columns<16; columns++){
+
+        for (rows =0; rows<16; rows++){
+            const container = document.querySelector('#container');
+            const tile = document.createElement('div');
+            tile.className = "etch";
+            container.appendChild(tile);
+        }
+    } 
 }
 
 //create array of all tiles 
-const tiles = document.getElementsByClassName('etch');
-console.log(tiles);
-let tileCount = tiles.length;
+const tilesList = document.getElementsByClassName('etch');
+console.log(tilesList);
+let tileCount = tilesList.length;
 
-drawThings(tiles);
-
-// add event listener for mousenter to every tile 
-function drawThings (tile){
-    for (k = 0; k < tileCount; k++) {
-     tile[k].addEventListener("mouseenter", function(event){
+drawThings();
+function drawThings (){
+    for (let tile = 0; tile < tileCount; tile++) {
+        tilesList[tile].addEventListener("mouseenter", function(event){
         event.target.style.background = "blue";
         })
     }
 }
 
 function reset (){
-
+    for (let tile = 0; tile < tileCount; tile++) {
+        tilesList[tile].style.background = "grey";
+        }
 }
 
 
 function resize (){
+var newSize = prompt("How Many squares per side? (up to 100", "16");
 
+    if (newSize == 100){
+        alert("Please enter a number under 100");
+    }
+    else if (newSize < 1) {
+        alert("Please enter a larger number");
+    }
+    else {
+        alert("wicked");
+        document.getElementById('container').style.height = "700px";
+        document.getElementById('container').style.width = "700px";
+    }
+
+
+//document.getElementById('container').style.height = "700px";
+//
+// change grid size but not pixel size ? 
 }
 
 
 function toggleColor () {
-
 }
